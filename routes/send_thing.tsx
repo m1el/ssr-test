c48a1b86d7e0708305930a42241736b38f5f9b92
Igor null <m1el.2027@gmail.com>
2022-06-05 20:58:58 +0300

    add send_thing

---
+++ routes/send_thing.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Script from 'src/script';
+export const Page = ({path}) => {
+    return (
+       <html>
+           <head>
+               <Script async src="dynamic/test.tsx" />
+               <title>Test SSR path={path}</title>
+           </head>
+           <body>
+               Hello world from static!
+               <div id="root"></div>
+           </body>
+       </html>
+    )
+};
+
+export default Page;
